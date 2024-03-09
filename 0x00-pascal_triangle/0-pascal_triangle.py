9a251cd67faf71ae197e603c81fc6212c645b412
Nasir Ibrahim <nasir.moh.ibrahim@gmail.com>
2024-03-09 12:02:29 +0100

    task 1 completed

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,9 @@
 from math import comb
 
 def pascal_triangle(n):
+    """
+    This function returns a pascal triangle of height n in the form of a list of integer lists
+    """
     if n <= 0:
         return []
     elif n == 1:
